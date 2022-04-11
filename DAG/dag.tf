b12a661e50cd20aa99ed0b4efb413edde23caf42
Josh.harris <josh.harris@foundry4.com>
2022-04-11 15:07:14 +0100

    update dagprd

--- DAG/dag.tf
+++ DAG/dag.tf
@@ -21,23 +21,25 @@ resource "azurerm_resource_group" "rg-dagprd" {
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "asp-dagprd" {
   name                = "asp-dagprd"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg-dagprd.location
+  resource_group_name = azurerm_resource_group.rg-dagprd.name
   sku {
-    tier = ""
-    size = "F1"
+    tier = "premium"
+    size = "P1"
   }
 }
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
 resource "azurerm_app_service" "app-dagprd" {
   name                = "app-dagprd"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-  source_control {
-    repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
+  location            = azurerm_resource_group.rg-dagprd.location
+  resource_group_name = azurerm_resource_group.rg-dagprd.name
+  app_service_plan_id = azurerm_app_service_plan.asp-dagprd.id
+
+
+connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "server=tcp:sql-dagprd.database.windows.net;database=dagprdfull;UID=app;Encrypt=True;TrustServerCertificate=False;Authentication=Active Directory Interactive"
   }
+ 
 }
